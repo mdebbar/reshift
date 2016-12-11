ab2e0125b85a3f117bc7f1e2af58ba527c6b67da
Mouad Debbar <mouad.debbar@gmail.com>
2016-12-11 01:47:17 -0800

    Add an 'OR' matcher + more tests

--- src/matchers.js
+++ src/matchers.js
@@ -1,11 +1,11 @@
 const t = require('recast/lib/types').namedTypes
 
-const matchers = {}
-module.exports = matchers
+const m = {}
+module.exports = m
 
-// Add matchers for ast types
+// Create matchers for all ast types
 Object.keys(t).forEach(function createMatcherForType(type) {
-  matchers[type] = (conditions) => (node) => {
+  m[type] = (conditions) => (node) => {
     if (!t[type].check(node)) {
       return false
     }
@@ -16,15 +16,21 @@ Object.keys(t).forEach(function createMatcherForType(type) {
   }
 })
 
-// Add an array matcher
-matchers.each = (itemMatcher) => (propArray) => {
+// Add a matcher for every array item
+m.every = (itemMatcher) => (propArray) => {
   if (!Array.isArray(propArray)) {
     throw 'Trying to use an array matcher on a non-array'
   }
-  return propArray.every(itemMatcher)
+  return propArray.every((prop) => matchProp(prop, itemMatcher))
+}
+
+// Add an OR matcher
+m.or = (...matchers) => (prop) => {
+  return matchers.some((matcher) => matchProp(prop, matcher))
 }
 
 function matchNode(node, conditions) {
+  // TODO: [optimize] run literal conditions first since they are cheap.
   return Object.keys(conditions).every(
     (key) => matchProp(node[key], conditions[key])
   )
